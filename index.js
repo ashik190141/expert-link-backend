d1cd3e726c014ab09352cc9eb20e8682aef47300
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2023-10-14 21:51:33 +0600

    add service

--- index.js
+++ index.js
@@ -22,64 +22,71 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server  (optional starting in v4.7)
-      await client.connect();
+    await client.connect();
 
-      const usersCollection = client.db('expertLink').collection('users');
-      
+    const usersCollection = client.db('expertLink').collection('users');
+    const serviceCollection = client.db('expertLink').collection('services');
 
-      app.post('/users', async (req, res) => {
-          const user = req.body;
-          const result = await usersCollection.insertOne(user);
-          res.send(result);
-      })
 
-      /* Admin related api */
-        // check admin
-        app.get('/users/admin/:email', async (req, res) => {
-          const email = req?.params?.email;
-          const query = { email: email }
-          const user = await usersCollection.findOne(query);
-          const result = { isAdmin: user?.role === 'admin'}
-          res.send(result);
-      })
-      // manage users
-      app.get('/allUsers', async (req, res) => {
-        const result = await usersCollection.find().toArray();
-        //   for (let i = 0; i < result.length; i++){
-        //       console.log(result.status);
-        //   }
-        res.send(result);
-      })
-
-      app.patch('/users/makeConsultant/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const updatedDoc = {
-              $set: {
-                status: "confirm"
-              }
-            }
-            const result = await usersCollection.updateOne(filter, updatedDoc);
-            res.send(result);
-      })
-
-      /* Consultant related api */
-        // check consultant
-        app.get('/users/consultant/:email', async (req, res) => {
-          const email = req?.params?.email;
-          const query = { email: email }
-          const user = await usersCollection.findOne(query);
-          console.log(user)
-          const result = { isConsultant: user?.role === 'consultant' && user?.status === 'confirm'}
-          res.send(result);
-        })
-        //consultant details
-      app.get('/consultantdetails/:email', async (req, res) => {
-        const email = req.params.email;
-        const query = { email: email };
-        const result = await usersCollection.findOne(query);
-        res.send(result);
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     })
+
+    /* Admin related api */
+    // check admin
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req?.params?.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { isAdmin: user?.role === 'admin' }
+      res.send(result);
+    })
+    // manage users
+    app.get('/allUsers', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      //   for (let i = 0; i < result.length; i++){
+      //       console.log(result.status);
+      //   }
+      res.send(result);
+    })
+
+    app.patch('/users/makeConsultant/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: "confirm"
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    /* Consultant related api */
+    // check consultant
+    app.get('/users/consultant/:email', async (req, res) => {
+      const email = req?.params?.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      console.log(user)
+      const result = { isConsultant: user?.role === 'consultant' && user?.status === 'confirm' }
+      res.send(result);
+    })
+    //consultant details
+    app.get('/consultantdetails/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    })
+    // add service
+    app.post('/consultant/addservice', async(req, res)=>{
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService)
+      res.send(result)
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -91,9 +98,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send("Expert-link is Running");
+  res.send("Expert-link is Running");
 })
 
 app.listen(port, () => {
-    console.log('port no', port);
+  console.log('port no', port);
 })
